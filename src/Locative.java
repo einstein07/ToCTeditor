8132e6b9aa20ac0859a7adc6de0ba8499f87db8e
einstein07 <mkhsin035@myuct.ac.za>
2021-11-22 13:39:17 +0200

    Initial commit of Template part class

---
+++ src/Locative.java
@@ -0,0 +1,33 @@
+
+public class Locative extends TemplatePart {
+    private String label;
+    private String nextPart;
+
+    public Locative(String partName, String label, String nextPart) {
+        super(partName);
+        this.label = label;
+        this.nextPart = nextPart;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNextPart() {
+        return nextPart;
+    }
+
+    public void setNextPart(String nextPart) {
+        this.nextPart = nextPart;
+    }
+
+    public String getTurtle(){
+        return "<" + getPartName() + "> a toct:Locative\n" +
+                "    ; toct:hasLabel \"" + getLabel() + "\"\n" +
+                "    ; toct:hasNextPart <" + getNextPart() + "> .";
+    }
+}

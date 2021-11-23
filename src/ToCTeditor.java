b317bcaeb8943763c075f2949a49b0f33b40337e
einstein07 <mkhsin035@myuct.ac.za>
2021-11-23 11:56:59 +0200

    Added dummy Template Items for testing purposes.

--- src/ToCTeditor.java
+++ src/ToCTeditor.java
@@ -19,16 +19,16 @@ public class ToCTeditor {
         TemplateItems templateItems = new TemplateItems(frame);
         CreateItem createItem = new CreateItem(frame);
 
-        gui = new ViewThread(homeScreen, templateItems, createItem);
-
 
         dataModel = new DataModel();
+
         dataModel.addPart(new Slot("slot1", "s1", "concord1"));
         dataModel.addPart(new Concord("concord1", "c1", "subjectVerb", "root1"));
         dataModel.addPart(new Root("root1", "nke"));
 
-        controller = new ControlThread(dataModel);
-        controller.start();
+        /**controller = new ControlThread(dataModel);
+        controller.start();*/
+        gui = new ViewThread(homeScreen, templateItems, createItem, dataModel);
         gui.start();
 
     }

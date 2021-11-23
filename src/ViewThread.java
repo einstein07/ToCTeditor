be1382865fdabd8a87f3c86ab35225e52082b764
einstein07 <mkhsin035@myuct.ac.za>
2021-11-23 11:44:59 +0200

    Added code to update/refresh gui according to user clicks of the toggle button.

--- src/ViewThread.java
+++ src/ViewThread.java
@@ -8,9 +8,10 @@
  * @(#) $Id$
  */
 public class ViewThread extends Thread {
-    CreateTemplate homeScreen;
-    TemplateItems templateItems;
-    CreateItem createItem;
+    private CreateTemplate homeScreen;
+     TemplateItems templateItems;
+    private CreateItem createItem;
+    private DataModel dataModel;
 
     boolean callCreateTemplate;
     boolean callTemplateItems;
@@ -18,33 +19,72 @@ public class ViewThread extends Thread {
 
     private TemplatePart currentPart;
 
-    public ViewThread(CreateTemplate homeScreen, TemplateItems templateItems, CreateItem createItem) {
+    private int index;
+    private int toggleBtnState;
+    private int prevToggleBtnState;
+
+    public ViewThread(CreateTemplate homeScreen, TemplateItems templateItems, CreateItem createItem, DataModel datamodel) {
         this.homeScreen = homeScreen;
         this.templateItems = templateItems;
         this.createItem = createItem;
+        this.dataModel = datamodel;
 
         this.callCreateTemplate = false;
         this.callTemplateItems = false;
         this.callCreateItem = false;
 
+        // By default, set the gui to show the turtle preview
+        this.toggleBtnState = 1;
+        this.prevToggleBtnState = 1;
+
+
+        //this.index = -1;
         homeScreen.setupGUI();
+    }
+
+    public int getToggleBtnState() {
+        return toggleBtnState;
+    }
+
+    public void setToggleBtnState(int toggleBtnState) {
+        this.toggleBtnState = toggleBtnState;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public TemplatePart getCurrentPart() {
+        currentPart = dataModel.getPart(index);
+        return currentPart;
     }
 
     @Override
     public void run() {
+        System.out.println("ToCTeditor v 1.0");
         while (true) {
-            currentPart = ToCTeditor.controller.getCurrentPart();
+            currentPart = dataModel.getPart(index);
             //System.out.println("Called template items equals: " + callTemplateItems);
             if (callTemplateItems) {
-                templateItems.setupGUI();
-                templateItems.getPartPanelEditor(currentPart);
+                currentPart = dataModel.getPart(index);
+                templateItems.setupGUI(templateItems.getPartPanelEditor(currentPart), templateItems.getPartPanelTurtle(currentPart));
                 callTemplateItems = false;
             } else if (callCreateItem) {
+                currentPart = dataModel.getPart(index);
                 createItem.setupGUI();
                 callCreateItem = false;
             } else if (callCreateTemplate) {
+                currentPart = dataModel.getPart(index);
                 homeScreen.setupGUI();
                 callCreateTemplate = false;
+            }
+            else if (toggleBtnState != prevToggleBtnState){
+                ToCTeditor.gui.templateItems.updateEditorTurtlePanel(templateItems.getPartPanelEditor(currentPart), templateItems.getPartPanelTurtle(currentPart));
+                prevToggleBtnState = toggleBtnState;
             }
 
 

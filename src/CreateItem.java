054e9750c5e33d81987bd9f73c670c2196628c09
einstein07 <mkhsin035@myuct.ac.za>
2021-11-23 11:43:54 +0200

    Populated the Create-New-Item panel as well as populated the Select-From-Existing-Items using the List of template parts storeed in the DataModel class.

--- src/CreateItem.java
+++ src/CreateItem.java
@@ -1,8 +1,3 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 /*
  * @(#) CreateItem.java   1.0   Nov 12, 2021
  *
@@ -12,6 +7,13 @@ import java.awt.event.ActionListener;
  *
  * @(#) $Id$
  */
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
 public class CreateItem {
     private int frameX;
     private int frameY;
@@ -49,21 +51,11 @@ public class CreateItem {
         pnlItemsTitle.setLayout(new BoxLayout(pnlItemsTitle, BoxLayout.LINE_AXIS));
         pnlItemsTitle.setMaximumSize(new Dimension(700,15));
         pnlItemsTitle.setAlignmentX(Component.CENTER_ALIGNMENT);
-        JLabel label = new JLabel("Template Items");
+        JLabel label = new JLabel("Create item");
         pnlItemsTitle.setBackground(Color.lightGray);
         label.setFont(new Font("Sans", Font.PLAIN, 15));
 
         pnlItemsTitle.add(label);
-
-        /**
-         * Items panel
-         */
-        JPanel pnlItems = new JPanel();
-        pnlItems.setLayout(new BoxLayout(pnlItems, BoxLayout.LINE_AXIS));
-        pnlItems.setMaximumSize(new Dimension(700,100));
-        pnlItems.setBackground(Color.lightGray);
-        pnlItems.setAlignmentX(Component.CENTER_ALIGNMENT);
-        pnlItems.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
 
         /**
          * Panel for Existing item selection title
@@ -75,9 +67,11 @@ public class CreateItem {
         pnlExistingItemTitle.setBackground(Color.lightGray);
 
 
-        JLabel lblExistingItemTitle = new JLabel("Select item from existing");
+        JLabel lblExistingItemTitle = new JLabel("Select from existing items");
         lblExistingItemTitle.setFont(new Font("Sans", Font.PLAIN, 15));
         lblExistingItemTitle.setMaximumSize(new Dimension(345,15));
+
+
 
         /**
          * Add title labels to labels panel
@@ -89,11 +83,33 @@ public class CreateItem {
          * Existing Item panel
          */
         JPanel pnlExistingItem = new JPanel();
-        pnlExistingItem.setLayout(new BoxLayout(pnlExistingItem, BoxLayout.LINE_AXIS));
+        pnlExistingItem.setLayout(new BoxLayout(pnlExistingItem, BoxLayout.Y_AXIS));
         pnlExistingItem.setMaximumSize(new Dimension(700,250));
         pnlExistingItem.setAlignmentX(Component.CENTER_ALIGNMENT);
         pnlExistingItem.setBackground(Color.lightGray);
         pnlExistingItem.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+
+        JPanel pnlSearchField = new JPanel();
+        pnlSearchField.setLayout(new BoxLayout(pnlSearchField, BoxLayout.LINE_AXIS));
+        pnlSearchField.setMaximumSize(new Dimension(700,30));
+        pnlSearchField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlSearchField.setBackground(Color.LIGHT_GRAY);
+
+        /**
+         * Search part text field
+         */
+        HintTextField txtSearch = new HintTextField("Search from existing item");
+        txtSearch.setAlignmentX(Component.CENTER_ALIGNMENT);
+        txtSearch.setMaximumSize(new Dimension(400,30));
+        txtSearch.setFont(new Font("Sans", Font.PLAIN, 14));
+
+        pnlSearchField.add(Box.createRigidArea(new Dimension(150,0)));
+        pnlSearchField.add(txtSearch);
+
+        pnlExistingItem.add(Box.createRigidArea(new Dimension(0,5)));
+        pnlExistingItem.add(pnlSearchField);
+
+        pnlExistingItem.add(setupExistingItems(ToCTeditor.dataModel.getData()));
 
 
         /**
@@ -107,8 +123,7 @@ public class CreateItem {
         btnBack.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 //[snip]
-                TemplateItems obj = new TemplateItems(frame/**, controlThread*/);
-                obj.setupGUI();
+                ToCTeditor.gui.setCallTemplateItems(true);
             }
         });
 
@@ -141,7 +156,7 @@ public class CreateItem {
         listPane.add(pnlItemsTitle);
         listPane.add(Box.createRigidArea(new Dimension(0,5)));
         // Add template name text field to list panel
-        listPane.add(pnlItems);
+        listPane.add(createPartOptions());
         listPane.add(Box.createRigidArea(new Dimension(0,10)));
         listPane.add(pnlExistingItemTitle);
         listPane.add(Box.createRigidArea(new Dimension(0,5)));
@@ -163,5 +178,151 @@ public class CreateItem {
 
 
         frame.setContentPane(g);
+    }
+
+    public JComponent createTemplateItem(String type){
+
+        JButton btnType = new JButton(type);
+        btnType.setFont(new Font("Sans", Font.PLAIN, 15));
+        btnType.setAlignmentX(Component.CENTER_ALIGNMENT);
+        btnType.setAlignmentY(Component.CENTER_ALIGNMENT);
+        btnType.setMaximumSize(new Dimension(150, 50));
+        btnType.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createEmptyBorder(5, 5, 5, 5),
+                BorderFactory.createLineBorder(Color.BLUE, 2)));
+
+        btnType.setBackground(Color.white);
+        return btnType;
+    }
+
+    public JPanel createPartOptions(){
+        JPanel pnlItems = new JPanel();
+        pnlItems.setLayout(new BoxLayout(pnlItems, BoxLayout.Y_AXIS));
+        pnlItems.setMaximumSize(new Dimension(700,170));
+        pnlItems.setBackground(Color.lightGray);
+        pnlItems.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pnlItems.setBorder(BorderFactory.createLineBorder(Color.GRAY, 1));
+
+        JPanel pnlRow1 = new JPanel();
+        pnlRow1.setLayout(new BoxLayout(pnlRow1, BoxLayout.LINE_AXIS));
+        pnlRow1.setMaximumSize(new Dimension(700,50));
+        pnlRow1.setBackground(Color.lightGray);
+        pnlRow1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        pnlRow1.add(Box.createRigidArea(new Dimension(43,0)));
+        pnlRow1.add(createTemplateItem("Concord"));
+        pnlRow1.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow1.add(createTemplateItem("Copula"));
+        pnlRow1.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow1.add(createTemplateItem("Locative"));
+        pnlRow1.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow1.add(createTemplateItem("Phrase"));
+        pnlRow1.add(Box.createRigidArea(new Dimension(5,0)));
+
+        JPanel pnlRow2 = new JPanel();
+        pnlRow2.setLayout(new BoxLayout(pnlRow2, BoxLayout.LINE_AXIS));
+        pnlRow2.setMaximumSize(new Dimension(700,50));
+        pnlRow2.setBackground(Color.lightGray);
+        pnlRow2.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        pnlRow2.add(Box.createRigidArea(new Dimension(43,0)));
+        pnlRow2.add(createTemplateItem("Polymorphic word"));
+        pnlRow2.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow2.add(createTemplateItem("Punctuation"));
+        pnlRow2.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow2.add(createTemplateItem("Root"));
+        pnlRow2.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow2.add(createTemplateItem("Slot"));
+        pnlRow2.add(Box.createRigidArea(new Dimension(5,0)));
+
+        JPanel pnlRow3 = new JPanel();
+        pnlRow3.setLayout(new BoxLayout(pnlRow3, BoxLayout.LINE_AXIS));
+        pnlRow3.setMaximumSize(new Dimension(700,50));
+        pnlRow3.setBackground(Color.lightGray);
+        pnlRow3.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        pnlRow3.add(Box.createRigidArea(new Dimension(43,0)));
+        pnlRow3.add(createTemplateItem("Unimorphic affix"));
+        pnlRow3.add(Box.createRigidArea(new Dimension(5,0)));
+
+        pnlRow3.add(createTemplateItem("Unimorphic word"));
+        pnlRow3.add(Box.createRigidArea(new Dimension(5,0)));
+
+
+        pnlItems.add(Box.createRigidArea(new Dimension(0,5)));
+        pnlItems.add(pnlRow1);
+        pnlItems.add(Box.createRigidArea(new Dimension(0,5)));
+        pnlItems.add(pnlRow2);
+        pnlItems.add(Box.createRigidArea(new Dimension(0,5)));
+        pnlItems.add(pnlRow3);
+        return pnlItems;
+    }
+
+    public JComponent setupExistingItems(List<TemplatePart> list) {
+        Box box = Box.createVerticalBox();
+        box.setBackground(Color.lightGray);
+        box.setMaximumSize(new Dimension(400, 200));
+        JScrollPane pnlScroll = new JScrollPane(box, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        pnlScroll.setMaximumSize(new Dimension(400, 200));
+        pnlScroll.setBackground(Color.lightGray);
+
+        /**DragMouseAdapter dh = new DragMouseAdapter();
+        box.addMouseListener(dh);
+        box.addMouseMotionListener(dh);*/
+
+        for (TemplatePart part : list) {
+            JLabel name = new JLabel(part.getPartName());
+            JLabel type = new JLabel(ToCTeditor.gui.templateItems.getPartType(part));
+            box.add(createItemComponent(name, type));
+        }
+        JPanel p = new JPanel();
+        p.setMaximumSize(new Dimension(700, 200));
+        p.setLayout(new BoxLayout(p, BoxLayout.LINE_AXIS));
+        p.setBackground(Color.lightGray);
+        p.add(Box.createRigidArea(new Dimension(150,0)));
+        p.add(pnlScroll);
+        return p;
+    }
+    private static JComponent createItemComponent(JComponent name, JComponent type) {
+        JPanel p = new JPanel();
+        p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+        p.setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createEmptyBorder(5, 5, 5, 5),
+                BorderFactory.createLineBorder(Color.BLUE, 2)));
+
+        JPanel pnlName = new JPanel();
+        pnlName.setLayout(new BoxLayout(pnlName, BoxLayout.Y_AXIS));
+        pnlName.setMaximumSize(new Dimension(400,50));
+        pnlName.setBackground(Color.white);
+        name.setFont(new Font("Sans", Font.PLAIN, 14));
+        name.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        pnlName.add(name);
+
+        type.setFont(new Font("Sans", Font.PLAIN, 8));
+        type.setMaximumSize(new Dimension(400,50));
+
+
+        JPanel pnlType = new JPanel();
+        pnlType.setLayout(new BoxLayout(pnlType, BoxLayout.LINE_AXIS));
+        pnlType.setMaximumSize(new Dimension(400,50));
+        pnlType.add(type);
+        pnlType.add(Box.createRigidArea(new Dimension(60,0)));
+        pnlType.setBackground(Color.white);
+
+
+
+        p.setMaximumSize(new Dimension(400, 50));
+        p.setMinimumSize(new Dimension(400, 50));
+        p.add(pnlName);
+        p.add(pnlType);
+        p.setBackground(Color.lightGray);
+        return p;
     }
 }
